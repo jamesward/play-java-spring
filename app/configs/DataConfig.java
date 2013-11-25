81f2e4a940efb651dcec5ef4a70a81ec7e911b0b
James Ward <james@jamesward.org>
2013-11-25 14:42:51 -0700

    update config and add user / password

--- app/configs/DataConfig.java
+++ app/configs/DataConfig.java
@@ -46,6 +46,8 @@ public class DataConfig {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(Play.application().configuration().getString("db.default.driver"));
         dataSource.setUrl(Play.application().configuration().getString("db.default.url"));
+        dataSource.setUsername(Play.application().configuration().getString("db.default.user"));
+        dataSource.setPassword(Play.application().configuration().getString("db.default.password"));
         return dataSource;
     }
 
