1c5063d0a3859604e2799693c2f47990c873e1ef
wmeyers <wmeyers@sofi.org>
2014-07-31 12:28:07 -0700

    Make in-memory database work for test configuration

--- test/TestDataConfig.java
+++ test/TestDataConfig.java
@@ -22,7 +22,7 @@ public class TestDataConfig extends DataConfig {
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:testdb"); // todo: why doesn't in-memory work here?
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
         return dataSource;
     }
     
